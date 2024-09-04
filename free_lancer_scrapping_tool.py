7d3111599187a0f98c1cb3d4919dd92efa80a83c
Navneeth R <90860785+NavneethR@users.noreply.github.com>
2024-09-04 06:31:54 +0530

    Update free_lancer_scrapping_tool.py

--- free_lancer_scrapping_tool.py
+++ free_lancer_scrapping_tool.py
@@ -1,34 +1,37 @@
 import requests
-import win10toast
+import subprocess
 from bs4 import *
 
-html = BeautifulSoup(requests.get("https://www.freelancer.com/job-search/projects/?projectLanguages=en").content,"html.parser")
-Toaster = win10toast.ToastNotifier()
-req_skills = ["Python"]
+html = BeautifulSoup(requests.get(
+    "https://www.freelancer.com/job-search/projects/?projectLanguages=en").content, "html.parser")
+req_skills = ["JavaScript"]
 all_jobs = []
 valid_jobs = []
 job_num = 1
 
+
 def add_job(job):
-    global all_jobs,job_num
+    global all_jobs, job_num
     skills = []
-    for skill in job.find_all(class_ = "JobSearchCard-primary-tagsLink"):
+    for skill in job.find_all(class_="JobSearchCard-primary-tagsLink"):
         skills.append(skill.text)
-    descr = job.find("p",class_ = "JobSearchCard-primary-description").text.strip()
+    descr = job.find(
+        "p", class_="JobSearchCard-primary-description").text.strip()
     job = job.find("a").text.strip()
     skill = skills
-    all_jobs.append([job,descr,skill])
-  
+    all_jobs.append([job, descr, skill])
+
+
 def validate_job(all_jobs):
-    for job,descr,skill in all_jobs:
+    for job, descr, skill in all_jobs:
         for req_skill in req_skills:
             if req_skill in skill:
-                valid_jobs.append([job,descr,skill])
+                valid_jobs.append([job, descr, skill])
     return valid_jobs
-        
-for job in html.find_all(class_ = "JobSearchCard-primary"):
+
+
+for job in html.find_all(class_="JobSearchCard-primary"):
     add_job(job)
 
 for job in validate_job(all_jobs):
-    Toaster.show_toast(job[0],job[1])
-
+    subprocess.Popen(["notify-send", job[0]+"\n"+job[1]])
